b314d323e8211378f776b0a51d8d73a04625e904
Evgzek <zekamusijhcuk@gmail.com>
2022-05-18 03:17:43 +0900

    add: read file

--- src/main/java/ru/bgpu/task/map/Main.java
+++ src/main/java/ru/bgpu/task/map/Main.java
@@ -1,7 +1,22 @@
 package ru.bgpu.task.map;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ok");
+        try {
+            FileInputStream fstream = new FileInputStream("C:\\Users\\79991\\Desktop\\access.log");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine;
+            br.readLine();
+            fstream.close();
+//            while ((br.readLine()) != null){
+//               System.out.println(strLine);
+//            }
+//            fstream.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
