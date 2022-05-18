2dd5235e0363e4f02145da5a9b8d3170a15b6276
Evgzek <zekamusijhcuk@gmail.com>
2022-05-19 03:37:08 +0900

    add: traffic counting for each IP

--- src/main/java/ru/bgpu/task/map/Main.java
+++ src/main/java/ru/bgpu/task/map/Main.java
@@ -7,23 +7,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try {
-            FileInputStream fstream = new FileInputStream("C:\\Users\\79991\\Desktop\\test.txt");
+            FileInputStream fstream = new FileInputStream("C:\\Users\\79991\\Desktop\\access.log");
             BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            HashMap<String, Integer> map = new HashMap<String, Integer>();
             String strLine;
-            HashMap<String, String> map = new HashMap<String, String>();
             br.readLine();
             while ((strLine = br.readLine()) != null){
                 String[] parts = strLine.split("\\s+");
-//                System.out.println(parts[2] + " " + parts[4]);
-//                String key = parts[2];
-//                String value = parts[4];
-//                map.put(key, value);
-
+                String key = parts[2];
+                int value = Integer.parseInt(parts[4]);
+                if (map.containsKey(key)){
+                    int p = map.get(key);
+                    value = value + p;
+                }
+                map.put(key, value);
             }
-
-//            for (String key : map.keySet()){
-//                System.out.println(key + " " + map.get(key));
-//            }
+            for (String key : map.keySet()){
+                System.out.println(key + " " + map.get(key));
+            }
             fstream.close();
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
